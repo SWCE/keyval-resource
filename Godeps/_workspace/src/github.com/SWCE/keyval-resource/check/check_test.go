0a283ed0fb8f6fe1a039d64615068b369cdb03cc
Roi Ezra <ezraroi@gmail.com>
2018-01-15 10:55:00 +0200

    fixed build

---
+++ Godeps/_workspace/src/github.com/SWCE/keyval-resource/check/check_test.go
@@ -0,0 +1,97 @@
+package main_test
+
+import (
+	"encoding/json"
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/onsi/gomega/gexec"
+
+	"github.com/SWCE/keyval-resource/models"
+	"github.com/onsi/gomega/gbytes"
+)
+
+var _ = Describe("Check", func() {
+	var (
+		checkCmd *exec.Cmd
+	)
+
+	BeforeEach(func() {
+		checkCmd = exec.Command(checkPath)
+	})
+
+	Context("when executed", func() {
+		var source map[string]interface{}
+		var version *models.EmptyVersion;
+		var response models.CheckResponse
+
+		BeforeEach(func() {
+			source = map[string]interface{}{}
+			response = models.CheckResponse{}
+			version = nil
+		})
+
+		JustBeforeEach(func() {
+			stdin, err := checkCmd.StdinPipe()
+			Expect(err).NotTo(HaveOccurred())
+
+			session, err := gexec.Start(checkCmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+
+			err = json.NewEncoder(stdin).Encode(map[string]interface{}{
+				"source":  source,
+				"version": version,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			<-session.Exited
+			Expect(session.ExitCode()).To(Equal(0))
+
+			err = json.Unmarshal(session.Out.Contents(), &response)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		Context("when no version is given", func() {
+			It("outputs an empty version array", func() {
+				Expect(response).To(HaveLen(0))
+			})
+		})
+
+		Context("when version is given", func() {
+
+			BeforeEach(func() {
+				version = &models.EmptyVersion{
+				}
+			})
+
+			It("outputs an empty version array", func() {
+				Expect(response).To(HaveLen(0))
+			})
+		})
+
+	})
+
+	Context("with invalid inputs", func() {
+		var session *gexec.Session
+
+		JustBeforeEach(func() {
+			stdin, err := checkCmd.StdinPipe()
+			Expect(err).NotTo(HaveOccurred())
+
+			session, err = gexec.Start(checkCmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+
+			stdin.Close()
+		});
+		Context("with a missing everything", func() {
+			It("returns an error", func() {
+				<-session.Exited
+				Expect(session.Err).To(gbytes.Say("parse error: EOF"))
+				Expect(session.ExitCode()).To(Equal(1))
+			})
+		})
+
+	})
+});
---
+++ Godeps/_workspace/src/github.com/SWCE/keyval-resource/in/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/SWCE/keyval-resource/models"
+	"fmt"
+	"bufio"
+	"sort"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fatalNoErr("usage: " + os.Args[0] + " <destination>")
+	}
+
+	destination := os.Args[1]
+
+	log("creating destination dir " + destination)
+	err := os.MkdirAll(destination, 0755)
+	if err != nil {
+		fatal("creating destination", err)
+	}
+
+	output := filepath.Join(destination, "keyval.properties")
+	log("creating output file " + output)
+	file, err := os.Create(output)
+	if err != nil {
+		fatal("creating output file", err)
+	}
+
+	defer file.Close()
+
+	var request models.InRequest
+
+	err = json.NewDecoder(os.Stdin).Decode(&request)
+	if err != nil {
+		fatal("reading request", err)
+	}
+
+	var inVersion = request.Version
+
+	w := bufio.NewWriter(file)
+
+	var keys []string
+	for k := range inVersion {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	log("writing " + strconv.Itoa(len(keys)) + " keys to output file")
+	for _, k := range keys {
+		fmt.Fprintf(w, "%s=%s\n", k, inVersion[k])
+	}
+
+	err = w.Flush()
+
+	if err != nil {
+		fatal("writing output file", err)
+	}
+
+	json.NewEncoder(os.Stdout).Encode(models.InResponse{
+		Version:  inVersion,
+	})
+
+	log("Done")
+}
+
+func fatal(doing string, err error) {
+	fmt.Fprintln(os.Stderr, "error " + doing + ": " + err.Error())
+	os.Exit(1)
+}
+
+func log(doing string) {
+	fmt.Fprintln(os.Stderr, doing)
+}
+
+func fatalNoErr(doing string) {
+	log(doing)
+	os.Exit(1)
+}
+
+
